eaed4020f2199fb3069c18e3bf0b232908396c55
AngeOsobo <angeosobo243@gmail.com>
2024-02-08 12:58:19 +0100

    Tp_Algo
    Tp_Alo_class Fraction

---
+++ Devoir_1_Algo.py
@@ -0,0 +1,32 @@
+class Fraction:
+    def __init__(self, num, den):
+        assert isinstance(num, int) and isinstance(den, int) and den > 0, "Le dénominateur doit être un entier strictement positif."
+        self.num = num
+        self.den = den
+
+    def __str__(self):
+        if self.den == 1:
+            return str(self.num)
+        else:
+            return f"{self.num}/{self.den}"
+
+    def eq(self, other):
+        return self.num * other.den == other.num * self.den
+
+# Création des instances F1, F2, F3, et F4
+F1 = Fraction(3, 4)
+F2 = Fraction(-8, 1)
+F3 = Fraction(2, 3)
+F4 = Fraction(21, 28)
+
+# Affichage des instances à  l'aide de la méthode spéciale str
+print(F1)
+print(F2)
+print(F3)
+print(F4)
+
+# Tests de l'égalité entre les fractions
+print(F1.eq(F2))  # False
+print(F1.eq(F3))  # False
+print(F1.eq(F4))  # False
+print(F3.eq(F4))  # True
